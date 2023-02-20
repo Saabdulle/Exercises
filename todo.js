e390b74c8b9a98c015c60d8ee707eefce533dac1
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-02-20 10:43:05 +0000

    latest update on task function

--- todo.js
+++ todo.js
@@ -1,14 +1,27 @@
+const newTaskInput = document.getElementById('newTaskInput')
+const todoList = document.getElementById('todo-list')
+const taskButton = document.getElementById('task-button')
+
+
+
 function task(newTask) {
     const taskItem = document.createElement('li');
     taskItem.setAttribute('class', 'task-input');
     
-    const addNewTask = document.createElement('class');
+    const addNewTask = document.createElement('div');
     addNewTask.setAttribute('class', 'new-task-button')
+
 
     const newTaskInfo = document.createElement('span');
     newTaskInfo.setAttribute('class', 'task-info');
 
-    newTaskInfo.appendChild(newTask)
+    newTaskInfo.innerText = newTask;
+
+    todoList.appendChild(taskItem)
+
+    taskItem.appendChild(taskButton)
+
+    taskItem.appendChild(newTaskInfo)
 
 }
-task(newTask)
+
