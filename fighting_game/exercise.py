fbf8ee1f5248ec7337bb7f64de6aec8529948b79
Saeed Abdulle <104086456+Saabdulle@users.noreply.github.com>
2023-03-13 17:00:32 +0000

    latest commit

--- fighting_game/exercise.py
+++ fighting_game/exercise.py
@@ -2,9 +2,26 @@ characters = ["a", "b", "c", "d", "e"]
 
 
 class Game:
-    user_input = input("Choose p1 or p2 ? ")
-    if user_input == "p1":
-        user_input = input("choose a character:")
+    def play(self):
+        print("Welcome to the game! ")
+        print("Please choose a player: 1 || 2 ? ")
+        player_choice = int(input())
+        if player_choice == "p1":
+            print("Choose a character: ")
+            character_choice = int(input())
+            for character in characters:
+                if character_choice == character:
+                    print("Choose your attack move: ")
+                    attack_choice = int(input())
+        elif player_choice == "p2":
+            characters_choice = int(input())
+        print("Choose a character: ")
+        character_choice = int(input())
         for character in characters:
-            if user_input == character:
-            user_input = input("Choose your attack move: ")
+            if character_choice == character:
+                print("Choose your defense move: ")
+                attack_choice = int(input())
+
+
+game = Game()
+game.play()
